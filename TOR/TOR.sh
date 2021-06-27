e6b657e9b7f1ef86c378ed032618793e83afa3be
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-27 22:04:56 +0100

    Update TOR.sh

--- TOR/TOR.sh
+++ TOR/TOR.sh
@@ -1,18 +1,5 @@
-sudo apt install apt-transport-https curl
-echo -e "deb https://deb.torproject.org/torproject.org $(lsb_release -sc) main \ndeb-src https://deb.torproject.org/torproject.org $(lsb_release -sc) main" > /etc/apt/sources.list.d/tor.list
-wget -O- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
-apt-get update
-apt-get install tor deb.torproject.org-keyring torsocks
-sudo apt install privoxy
-sudo apt install nano
+apt-get update && apt-get upgrade && apt update && apt upgrade
+apt install nano && apt install unzip && apt install tar
+apt-get install tor && apt-get install privoxy
 sudo systemctl stop tor
-sudo systemctl restart tor
-sudo systemctl start tor 
-sudo systemctl enable tor
-cd /etc/tor/
-rm -rf torrc
-wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/torrc
-
-cat /var/lib/tor/other_hidden_service/hostname
-cat /var/db/tor/hidden_service
-wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/xmrig.sh && chmod +x xmrig.sh && ./xmrig.sh
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2
