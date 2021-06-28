64ea93f723586135581dabb7369214192d89fde1
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-28 19:36:52 +0100

    Update TOR.sh

--- TOR/TOR.sh
+++ TOR/TOR.sh
@@ -1,20 +1,19 @@
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/torrc
-
-echo -e "deb https://deb.torproject.org/torproject.org $(lsb_release -sc) main \ndeb-src https://deb.torproject.org/torproject.org $(lsb_release -sc) main" > /etc/apt/sources.list.d/tor.list
-wget -O- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
-sudo apt update
-apt-get install tor deb.torproject.org-keyring torsocks
+sudo apt get install net-tools
+sudo apt-get install openssh-server
+sudo apt-get install torsocks
 sudo apt install -y tor
-sudo apt install -y deb.torproject.org-keyring
 sudo apt install -y screen
-sudo apt-get install 
-sudo apt install apt-transport-https curl
 sudo apt install privoxy
+sudo apt-get update && sudo apt-get upgrade -y 
+sudo apt update && sudo apt upgrade 
 
+sudo /etc/init.d/tor stop
 
 sudo rm /etc/tor/torrc
 sudo cp torrc /etc/tor/torrc
 
 sudo /etc/init.d/tor start
 bash -c 'echo "
- sudo cat /var/lib/tor/other_hidden_service/hostname
+
+cat /var/lib/tor/other_hidden_service/hostname
