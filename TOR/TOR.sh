c2c519e3fe467598c0204c35493998fd439e993b
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-28 18:30:38 +0100

    Delete TOR.sh

--- TOR/TOR.sh
+++
@@ -1,5 +0,0 @@
-apt-get update && apt-get upgrade && apt update && apt upgrade
-apt install nano && apt install unzip && apt install tar
-apt-get install tor && apt-get install privoxy
-sudo systemctl stop tor
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2
