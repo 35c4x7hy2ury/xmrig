74db275504d47e901d17db4dc8063cad9d20c137
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-28 19:15:03 +0100

    Update TOR.sh

--- TOR/TOR.sh
+++ TOR/TOR.sh
@@ -1,11 +1,16 @@
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/torrc
 
-gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo -E apt-key add -
+echo -e "deb https://deb.torproject.org/torproject.org $(lsb_release -sc) main \ndeb-src https://deb.torproject.org/torproject.org $(lsb_release -sc) main" > /etc/apt/sources.list.d/tor.list
+wget -O- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
 sudo apt update
+apt-get install tor deb.torproject.org-keyring torsocks
 sudo apt install -y tor
 sudo apt install -y deb.torproject.org-keyring
 sudo apt install -y screen
+sudo apt-get install 
+sudo apt install apt-transport-https curl
+sudo apt install privoxy
+
 
 sudo rm /etc/tor/torrc
 sudo cp torrc /etc/tor/torrc
@@ -13,3 +18,4 @@ sudo cp torrc /etc/tor/torrc
 
 sudo /etc/init.d/tor start
 bash -c 'echo "
+cat /var/lib/tor/other_hidden_service/hostname
