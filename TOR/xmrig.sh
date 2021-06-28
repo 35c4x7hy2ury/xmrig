ef1db6e794e079e8e9d3ca9df0ef26893f88f478
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-28 18:28:49 +0100

    Delete xmrig.sh

--- TOR/xmrig.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2 && rm -rf config.json && ls
-wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/config.json && ls && ./xmrig
