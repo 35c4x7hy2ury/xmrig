bb1a179c1c1b033f288dead48b76bac379cb103f
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-02 11:14:56 +0100

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,2 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2 && rm -rf config.json && ls
+wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/config.json && ls
