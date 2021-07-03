6bc11d251c958d3248b98fc39efdf97288287373
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-03 18:54:02 +0100

    Update onion.sh

--- TOR/onion.sh
+++ TOR/onion.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt update && sudo apt upgrade -y 
-sudo apt-get install nano -y && sudo apt-get install tor -y && sudo apt-get install privoxy -y
-sudo apt-get install --reinstall systemd
+sudo apt-get install tor -y && sudo apt-get install privoxy -y
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/torrc
 sudo /etc/init.d/tor stop
 sudo rm /etc/tor/torrc
