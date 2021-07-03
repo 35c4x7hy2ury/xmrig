a15acdd240605f24d7a5bca03846dfff4cfa7194
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-03 18:54:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt update && sudo apt upgrade -y 
+sudo apt-get install git build-essential cmake automake libtool autoconf
+sudo apt-get install libtool m4 automake -y
+sudo apt-get install gcc++ -y
+sudo apt-get install --reinstall systemd -y
+sudo apt-get install nano -y
+
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/onion.sh && chmod +x onion.sh && ./onion.sh
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/xmr.sh && chmod +x xmr.sh && ./xmr.sh
