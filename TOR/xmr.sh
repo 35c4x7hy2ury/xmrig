a37639ffbe0ead4773023482540a40594f644b81
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-03 18:22:27 +0100

    Update xmr.sh

--- TOR/xmr.sh
+++ TOR/xmr.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt update && sudo apt upgrade -y 
-sudo apt-get install git build-essential cmake automake libtool autoconf gcc++ gm4 gnum4 m4
+sudo apt-get install git build-essential cmake automake libtool autoconf
+sudo apt-get install libtool m4 automake
+sudo apt-get install libtool gm4 automake
+sudo apt-get install libtool gnum4 automake
+sudo apt-get install gcc++ 
+
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/scripts
 ./build_deps.sh && cd ../build
