67ba5b325af84289025b1e1913a185705b31b747
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-03 19:52:20 +0100

    Update xmr.sh

--- TOR/xmr.sh
+++ TOR/xmr.sh
@@ -5,7 +5,7 @@ mkdir xmrig/build && cd xmrig/scripts
 ./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
-
-cd /xmrig/build
-cp xmrig/build/xmrig  xmrig/xmrig
+ls
+cd ..
+cp build/xmrig xmrig
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/config.json && rm -rf bin  build  CHANGELOG.md  cmake  CMakeLists.txt  doc  LICENSE  package.json  README.md  res  scripts  src
