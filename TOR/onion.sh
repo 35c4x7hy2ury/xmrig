8fb6df0fa950d9ce487af2a7f31ae57a13f81c2f
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-03 17:27:41 +0100

    Update and rename TOR.sh to onion.sh

---
+++ TOR/onion.sh
@@ -0,0 +1,9 @@
+apt-get update && apt-get upgrade -y && apt update && apt upgrade -y 
+apt-get install nano -y && apt-get install tor -y && apt-get install privoxy -y
+apt-get install --reinstall systemd
+wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/torrc
+/etc/init.d/tor stop
+rm -rf /etc/tor/torrc
+cp torrc /etc/tor/torrc
+systemctl enable tor
+/etc/init.d/tor start
