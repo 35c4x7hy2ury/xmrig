7e5fbf4a0fa8c0d5a02debdc809a29a2985dee75
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-19 19:11:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt-get install --reinstall systemd -y
 sudo apt-get install nano -y
 
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/onion.sh && chmod +x onion.sh && ./onion.sh
-wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/xmr.sh && chmod +x xmr.sh && ./xmr.sh
+wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/xmr.sh && chmod +x xmr.sh && ./xmr.sh
 wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/config.sh && chmod +x config.sh && ./config.sh
 
 chmod +x xmrig && ./xmrig
