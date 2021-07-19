f36052d56dbb0dd644406b37e081a6544b9d8b8b
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-19 18:12:55 +0100

    Update config.sh

--- TOR/config.sh
+++ TOR/config.sh
@@ -4,6 +4,14 @@
 sed -i '75d' /etc/tor/torrc
 sed -i "75i\HiddenServicePort /var/lib/tor/other_hidden_service/hostname\n" /etc/tor/torrc
 sed -i "75i\HiddenServicePort 4444 pool.minexmr.com:4444\n" /etc/tor/torrc
+sed -i "75i\HiddenServicePort 4444 fr.minexmr.com:4444\n" /etc/tor/torrc
+sed -i "75i\HiddenServicePort 4444 de.minexmr.com:4444\n" /etc/tor/torrc
+sed -i "75i\HiddenServicePort 4444 sg.minexmr.com:4444\n" /etc/tor/torrc
+sed -i "75i\HiddenServicePort 4444 ca.minexmr.com:4444\n" /etc/tor/torrc
+sed -i "75i\HiddenServicePort 4444 us-west.minexmr.com:4444\n" /etc/tor/torrc
+
+sudo systemctl enable tor
+sudo /etc/init.d/tor start
 
 sed -i '4d' config.json
 head -1 /var/lib/tor/other_hidden_service/hostname | sed '1s/^/3i /' | sed -i -f- config.json
