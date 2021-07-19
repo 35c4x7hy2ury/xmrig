2e9e7825866802c5cae84d6b0676fc9e1885e0c5
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-19 18:07:08 +0100

    Update config.sh

--- TOR/config.sh
+++ TOR/config.sh
@@ -5,7 +5,7 @@ sed -i '75d' /etc/tor/torrc
 sed -i "75i\HiddenServicePort /var/lib/tor/other_hidden_service/hostname\n" /etc/tor/torrc
 sed -i "75i\HiddenServicePort 4444 pool.minexmr.com:4444\n" /etc/tor/torrc
 
-sed '4d' config.json
+sed -i '4d' config.json
 head -1 /var/lib/tor/other_hidden_service/hostname | sed '1s/^/3i /' | sed -i -f- config.json
 sed -i 4s/$/",/ /xmrig/config.json
-sed '4s|^|        "host": |' config.json
+sed -i '4s|^|        "host": |' config.json
