12cfcb14e8ab5ca0c82e2e03005d325f3fcf6e9e
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-19 19:10:30 +0100

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,2 +1,2 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz && tar xf xmrig-6.12.2-linux-x64.tar.gz && cd xmrig-6.12.2 && rm -rf config.json && ls
-wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/config.json && ls
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz && tar xf xmrig-6.13.1-linux-x64.tar.gz && cd xmrig-6.13.1 && rm -rf config.json && ls
+wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/config.json && ls
