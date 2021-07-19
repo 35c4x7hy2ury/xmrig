41f6e1b73bb26c2eae45a00f96d7d3274611b3ec
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-19 17:57:28 +0100

    Update onion.sh

--- TOR/onion.sh
+++ TOR/onion.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
 sudo apt-get install tor -y
-wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/torrc
 sudo /etc/init.d/tor stop
-sudo rm /etc/tor/torrc
-sudo cp torrc /etc/tor/torrc
-sudo systemctl enable tor
-sudo /etc/init.d/tor restart
-sudo /etc/init.d/tor start
