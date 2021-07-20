aaa7c01b08791fc056cdd4b0dab70648bb2c453d
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-20 20:11:47 +0100

    Update config.sh

--- TOR/config.sh
+++ TOR/config.sh
@@ -4,6 +4,6 @@ cd xmrig-6.13.1
 sed -i '4d' config.json
 cat /var/lib/tor/other_hidden_service/hostname | sed '1s/^/4i /' | sed -i -f- config.json
 sed -i '4s/$/",/ ' config.json 
-sed -i '4s|^|        "host": |' /content/xmrig-6.13.1/config.json
+sed -i '4s|^|        "host": "|' /content/xmrig-6.13.1/config.json
 
 chmod +x xmrig && ./xmrig
