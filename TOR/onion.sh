2eac78cef5c22416e801d36501e7551bf122d8b1
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-21 09:53:45 +0100

    Update onion.sh

--- TOR/onion.sh
+++ TOR/onion.sh
@@ -3,12 +3,15 @@ sudo apt-get install --reinstall systemd -y
 sudo apt-get install tor -y
 sudo service tor stop
 sudo apt-get install -y sed
+chmod +w /etc/tor/torrc
 
 sed -i '75s/#HiddenServiceDir/HiddenServiceDir/ ' /etc/tor/torrc
 sed -i '18s/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
 sed -i 's/#ORPort/ORPort/ ' /etc/tor/torrc
 sed -i '76s/#HiddenServicePort/HiddenServicePort/ ' /etc/tor/torrc
 sed -i '76s/80/8080/g ' /etc/tor/torrc
+
+chmod +x /etc/tor/torrc
 
 sudo systemctl enable tor
 sudo service tor start
