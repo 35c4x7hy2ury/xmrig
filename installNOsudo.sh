6fc0cf4887051f2ad542b250bb0f2151482b4742
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-21 08:30:21 +0100

    Update installNOsudo.sh

--- installNOsudo.sh
+++ installNOsudo.sh
@@ -1,6 +1,7 @@
-apt-get update -y &&  apt-get upgrade -y &&  apt update -y &&  apt upgrade -y
-
-apt-get install tor -y
+echo -e "deb https://deb.torproject.org/torproject.org $(lsb_release -sc) main \ndeb-src https://deb.torproject.org/torproject.org $(lsb_release -sc) main" > /etc/apt/sources.list.d/tor.list
+wget -O- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
+apt-get update
+apt-get install tor deb.torproject.org-keyring torsocks
 service tor stop
 
 sed -i '75s/#HiddenServiceDir/HiddenServiceDir/ ' /etc/tor/torrc
