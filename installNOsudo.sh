723d3f12fcc9aaae5344db06b47ecc2cff7ee838
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-21 08:41:43 +0100

    Update installNOsudo.sh

--- installNOsudo.sh
+++ installNOsudo.sh
@@ -1,4 +1,3 @@
-echo -e "deb https://deb.torproject.org/torproject.org $(lsb_release -sc) main \ndeb-src https://deb.torproject.org/torproject.org $(lsb_release -sc) main" > /etc/apt/sources.list.d/tor.list
 wget -O- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo apt-key add -
 apt-get update
 apt-get install tor deb.torproject.org-keyring torsocks
